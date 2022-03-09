b8065355077168b86947b451be18d6d73e964b41
Aurora <aurora.laghi@studio.unibo.it>
2022-03-09 18:00:43 +0100

    Exercises lab02

--- Lab02/src/main/scala/u02/Optionals.scala
+++ Lab02/src/main/scala/u02/Optionals.scala
@@ -20,6 +20,19 @@ object Optionals extends App : // questo si fa perchè estende un'applicazione e
       case Some(a) => f(a)
       case _ => None()
 
+    def filter[A](opt: Option[A])(pred: A => Boolean): Option[A] = opt match
+      case Some(a) if pred(a) => opt
+      case _ => None()
+
+    def map[A](opt: Option[A])(pred: A => Boolean): Option[Boolean] = opt match
+      case Some(a) if !pred(a) => Some(false)
+      case Some(a) => Some(true)
+      case _ => None()
+
+    def map2[A,B](opt1: Option[A])(opt2: Option[B]): Option[(A,B)] = (opt1,opt2) match
+      case (Some(a), Some(b)) => Some(a,b)
+      case _ => None()
+
   import Option.*
 
   val s1: Option[Int] = Some(1)
@@ -32,3 +45,17 @@ object Optionals extends App : // questo si fa perchè estende un'applicazione e
   println(flatMap(s1)(i => Some(i + 1))) // Some(2)
   println(flatMap(s1)(i => flatMap(s2)(j => Some(i + j)))) // Some(3)
   println(flatMap(s1)(i => flatMap(s3)(j => Some(i + j)))) // None
+
+  println(filter(Some(5))(_>2)) // Some(5)
+  println(filter(Some(5))(_>8)) // None
+  println(filter(None[Int]())(_>2)) // None
+
+  println(map(s1)(_>2)) // Some(false)
+  println(map(Some(5))(_>2)) // Some(true)
+  println(map(Some(5))(_>8)) // Some(false)
+  println(map(s3)(_>(-1))) // None
+  println(map(None[Int]())(_>2)) // None
+
+  println(map2(s1)(s2)) // Some((1,2))
+  println(map2(Some(20))(Some(30))) // Some((20,30))
+  println(map2(s1)(s3)) // None
