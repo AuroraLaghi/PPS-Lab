582e59ff7adc889b12d4e8a342e700ab3cb6edc6
Aurora <aurora.laghi@studio.unibo.it>
2022-03-10 18:53:18 +0100

    refactor exercises

---
+++ Lab02/src/test/scala/u02/Es6Test.scala
@@ -0,0 +1,21 @@
+package u02
+
+import org.junit.*
+import org.junit.Assert.*
+import u02.Es6.Figure.*
+
+class Es6Test:
+
+  val rectangle = Rectangle(5,2)
+  val circle = Circle(0)
+  val square = Square(6)
+
+  @Test def testPerimeter() =
+    assertEquals(14,perimeter(rectangle),0)
+    assertEquals(0,perimeter(circle),0)
+    assertEquals(24,perimeter(square),0)
+
+  @Test def testArea() =
+    assertEquals(10,area(rectangle),0)
+    assertEquals(0,area(circle),0)
+    assertEquals(36,area(square),0)
